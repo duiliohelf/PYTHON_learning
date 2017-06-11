ce3ab9587d2d3b717f59877b6b89cb3fa209526a
Duilio Helfenstens <duiliohelf@gmail.com>
2017-06-11 10:39:37 -0300

    teste

---
+++ Tabuada_de_um_numero.py
@@ -0,0 +1,14 @@
+#Este codigo resolve tabuadas de qualquer numero
+
+x = int(input('Digite um numero para ver sua tabuada:'))
+y = x
+z = 1
+print('-' * 12)
+
+while z != 11:
+    print('{} x {:2} = {}' .format(y, z, y*z ))
+    x -= z
+    z += 1
+
+
+print('-' * 12)
